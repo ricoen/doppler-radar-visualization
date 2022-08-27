499e123334f304e9f2777c87b8e58f6c975fa5c6
ricoen <rico.enrayan@gmail.com>
2022-08-27 19:01:29 +0700

    Changes

--- doppler-matplotlib.py
+++ doppler-matplotlib.py
@@ -4,51 +4,54 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import contextily as ctx
 import matplotlib.colors as mcolors
-from matplotlib.colors import ListedColormap
+# from matplotlib.colors import ListedColormap
 
 
-data_path = os.path.join("data", "KAMX_N0V_20210131_000000.shp")
+data_path = os.path.join("data", "TJUA_V06_20220810_000211.shp")
 data = gpd.read_file(data_path)
 
 df = data.to_crs(epsg=3857)
 
-color_index = df['colorIndex']
-sensed = df['value']
+# print(df["value"])
 
-vmin = color_index.min()
-vmax = color_index.max()
+sensed = df["value"]
 
 def plot_data(colormap):
+    vmin = sensed.min()
+    vmax = sensed.max()
+
     fig, ax = plt.subplots(figsize=(10, 10))
-    fig.suptitle('Miami Doppler Radar (KAMX), NEXRAD LEVEL III, Base Velocity', fontsize=16)
+    fig.suptitle("San Juan (TJUA), NEXRAD LEVEL II", fontsize=16)
     
-    df.plot(column='colorIndex',
+    df.plot(column="value",
             alpha=0.8,
             cmap=colormap,
             ax=ax)
     
-    ax.set_title('01/31/2021 00:01:01')
-    ax.set_xlabel('Long')
-    ax.set_ylabel('Lat')
+    ax.set_title("08/10/2022 00:02:11")
+    ax.set_xlabel("Long")
+    ax.set_ylabel("Lat")
     
     normalize = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=8, vmax=vmax)
     
     scalarmappaple = cm.ScalarMappable(norm=normalize, cmap=colormap)
-    scalarmappaple.set_array(color_index)
+    scalarmappaple.set_array(sensed)
     
     cbar = plt.colorbar(scalarmappaple)
-    cbar.set_ticks([vmin, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, vmax])
-    cbar.set_ticklabels(['-64', '-50', '-36', '-26', '-20', '-10', '-1', '0', 
-                         '10', '20', '26', '36', '50', '64', 'RF'])
-    cbar.set_label('kts')
+    # cbar.set_ticks([vmin, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, vmax])
+    # cbar.set_ticklabels(["-64", "-50", "-36", "-26", "-20", "-10", "-1", "0", 
+    #                      "10", "20", "26", "36", "50", "64", "RF"])
+    cbar.set_label("dBZ")
     
     ctx.add_basemap(ax, zoom=10, source=ctx.providers.Stamen.TonerLite)
     
     plt.show()
 
 
-cmap = ListedColormap(['#03dffc', '#0328fc', '#3903fc', '#03fca9', '#02d402', 
-                       '#005c00', '#a39c8e', '#5e5e5e', '#ff8c00', '#ffc800',
-                       '#f6ff00', '#8B0000', '#964B00', '#ff0000', '#4B0082'])
+# cmap = ListedColormap(["#03dffc", "#0328fc", "#3903fc", "#03fca9", "#02d402", 
+#                        "#005c00", "#a39c8e", "#5e5e5e", "#ff8c00", "#ffc800",
+#                        "#f6ff00", "#8B0000", "#964B00", "#ff0000", "#4B0082"])
+
+cmap = "jet"
 
 plot_data(cmap)
--- doppler-plotly.py
+++ doppler-plotly.py
@@ -4,29 +4,39 @@ import plotly.express as px
 import plotly.io as pio
 
 
-pio.renderers.default = "browser"
+pio.renderers.default = "firefox"
 
-data_path = os.path.join("data", "KAMX_N0V_20210131_000000.shp")
+data_path = os.path.join("data", "TJUA_V06_20220810_000211.shp")
 data = gpd.read_file(data_path)
 
 df = data.to_crs("WGS84")
+sensed = df['value']
 
-def plot_radar_data(color_map, vmin, vmax):
+def plot_radar_data():
+   vmin = sensed.min()
+   vmax = sensed.max()
+   
    fig = px.choropleth_mapbox(df, geojson = df.geometry, locations = df.index, 
-                              color = "colorIndex", 
-                              color_continuous_scale = color_map, 
+                              color = "value", 
+                              color_continuous_scale = "jet", 
                               range_color = (vmin, vmax), 
                               mapbox_style = "carto-positron",
-                              zoom = 9, center = {"lat": 25.6353, "lon": -80.4171},
-                              hover_data = ["value", "colorIndex"],
-                              labels={'colorIndex':'Color Index'}
+                              zoom = 10, center = {"lat": 18.122793, "lon": -66.082846},
+                              labels={'value':'dBZ'}
                            )
-   fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+   
+   fig.update_layout(margin = {"r":0,"t":0,"l":0,"b":0},
+                     title = {"text":"San Juan (TJUA), NEXRAD LEVEL II",
+                              "xanchor":"center",
+                              "yanchor":"top",
+                              "x":0.5})
+   fig.update_traces(
+      hovertemplate = None,
+      hoverinfo = 'skip'
+   )
+   
    pio.show(fig)
 
-color_index = df['colorIndex']
-palette = ['#03dffc', '#0328fc', '#3903fc', '#03fca9', '#02d402', '#005c00', 
-            '#a39c8e', '#5e5e5e', '#ff8c00', '#ffc800', '#f6ff00', '#8B0000', 
-            '#964B00', '#ff0000', '#4B0082']
+# palette = ["turbo"]
 
-plot_radar_data(palette, color_index.min(), color_index.max())
+plot_radar_data()
