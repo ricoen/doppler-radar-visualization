cd08e8281d6b0e90870f58e34cf0a74bd234d2f6
ricoen <rico.enrayan@gmail.com>
2022-09-27 19:11:13 +0700

    Changes

--- doppler-plotly.py
+++ doppler-plotly.py
@@ -16,23 +16,24 @@ def plot_radar_data():
    vmin = sensed.min()
    vmax = sensed.max()
    
-   fig = px.choropleth_mapbox(df, geojson = df.geometry, locations = df.index, 
-                              color = "value", 
-                              color_continuous_scale = "jet", 
-                              range_color = (vmin, vmax), 
-                              mapbox_style = "carto-positron",
-                              zoom = 10, center = {"lat": 18.122793, "lon": -66.082846},
+   fig = px.choropleth_mapbox(df, geojson=df.geometry, locations=df.index, 
+                              color="value", 
+                              color_continuous_scale="jet", 
+                              range_color=(vmin, vmax), 
+                              mapbox_style="carto-positron",
+                              zoom=9, center={"lat": 18.122793, "lon": -66.082846},
                               labels={'value':'dBZ'}
                            )
    
-   fig.update_layout(margin = {"r":0,"t":0,"l":0,"b":0},
-                     title = {"text":"San Juan (TJUA), NEXRAD LEVEL II",
+   fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+                     title={"text":"San Juan (TJUA), NEXRAD LEVEL II",
                               "xanchor":"center",
                               "yanchor":"top",
                               "x":0.5})
    fig.update_traces(
-      hovertemplate = None,
-      hoverinfo = 'skip'
+      hovertemplate=None,
+      hoverinfo='skip',
+      marker_line_width=0
    )
    
    pio.show(fig)
